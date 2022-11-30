98d8f60dc7bfd357116fc52d29a2507ce3730856
Jacob Ney <jacobney@colostate.edu>
2022-11-30 15:45:20 -0700

    JN playing with treelists after DRG Edits

--- main.py
+++ main.py
@@ -3,34 +3,28 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-#f = open('C:/Users/neyja/OneDrive/Documents/Bitvise_SSH/Temp4Servers/toDRG/treesrhof.dat')
-filename = os.path.basename('C:/Users/neyja/OneDrive/Documents/Bitvise_SSH/Temp4Servers/toDRG/treesrhof.dat')
-# filename for working directory: 'treesrhof.dat'
-
 def import_fortran_dat_file(filename, cell_nums):
     from scipy.io import FortranFile
     (nx,ny,nz) = cell_nums
     arr =  FortranFile(filename,'r','uint32').read_ints('float32').T.reshape(nz,ny,nx)
     return arr
 
-'''
-matplotlib.pyplot.imshow
+rhof = import_fortran_dat_file('treesrhof.dat',[10,10,16])
 
-with open('test.txt') as f:
-    lines = f.readlines()
-'''
-
-rhof = import_fortran_dat_file('treesrhof.dat',[400,400,26])
-
-print ('Test 1')
 plt.figure(1)
 plt.imshow(rhof[0,:,:],origin='lower')
 plt.colorbar()
-plt.savefig('surf.png')
-
+plt.savefig('surface.png')
+print('Surface')
 
 plt.figure(2)
 plt.imshow(rhof[5,:,:],origin='lower')
-plt.savefig('canop.png')
 plt.colorbar()
-print ('Test 2')
+plt.savefig('midstory.png')
+print('Midstory')
+
+plt.figure(3)
+plt.imshow(rhof[10,:,:],origin='lower')
+plt.colorbar()
+plt.savefig('canopy.png')
+print('Canopy')

aae475bf8ed2f17b8296f2e8a03788a53020d61f
Jacob Ney <jacobney@colostate.edu>
2022-12-02 15:46:42 -0700

    Treelist visualization
    w/ real treelist

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def import_fortran_dat_file(filename, cell_nums):
     arr =  FortranFile(filename,'r','uint32').read_ints('float32').T.reshape(nz,ny,nx)
     return arr
 
-rhof = import_fortran_dat_file('treesrhof.dat',[10,10,16])
+rhof = import_fortran_dat_file('treesrhof.dat',[850,350,26])
 
 plt.figure(1)
 plt.imshow(rhof[0,:,:],origin='lower')
@@ -18,13 +18,13 @@ plt.savefig('surface.png')
 print('Surface')
 
 plt.figure(2)
-plt.imshow(rhof[5,:,:],origin='lower')
+plt.imshow(rhof[10,:,:],origin='lower')
 plt.colorbar()
 plt.savefig('midstory.png')
 print('Midstory')
 
 plt.figure(3)
-plt.imshow(rhof[10,:,:],origin='lower')
+plt.imshow(rhof[24,:,:],origin='lower')
 plt.colorbar()
 plt.savefig('canopy.png')
 print('Canopy')
